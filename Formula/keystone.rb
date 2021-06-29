526a251b3a5c69c0cb76d60913bec8d2b60027d4
Gael PHILIPPE <gaelph@gmail.com>
2021-06-29 16:09:04 +0200

    fix(): enable develop build
    Use environement variables to enable a temporary developement build

--- Formula/keystone.rb
+++ Formula/keystone.rb
@@ -2,31 +2,39 @@ class Keystone < Formula
   desc 'Securely share application secret with your team'
   homepage 'https://keytone.sh'
   head 'https://github.com/wearedevx/keystone.git', branch: 'develop'
-  head 'https://github.com/wearedevx/keystone.git', branch: 'develop'
+  # url 'https://github.com/wearedevx/keystone.git', branch: 'develop', using: 'git'
   sha256 '4dbe8faff569b8992ee091a207367604db468648503e5e6679a4c89e0918d525'
 
   depends_on 'openssl'
+  depends_on 'go'
 
   def install_themis
     system 'git', 'clone', 'https://github.com/cossacklabs/themis.git'
-    Dir.chdir 'themis'
-
-    ENV['ENGINE'] = 'openssl'
-    ENV['ENGINE_INCLUDE_PATH'] = Formula['openssl'].include
-    ENV['ENGINE_LIB_PATH'] = Formula['openssl'].lib
-    ENV['PREFIX'] = prefix
-    system 'make', 'install'
-
-    Dir.chdir '..'
+    Dir.chdir 'themis' do
+      ENV['ENGINE'] = 'openssl'
+      ENV['ENGINE_INCLUDE_PATH'] = Formula['openssl'].include
+      ENV['ENGINE_LIB_PATH'] = Formula['openssl'].lib
+      ENV['PREFIX'] = prefix
+      system 'make', 'install'
+    end
   end
   
   def install
     install_themis()
 
     ENV['CGO_ENABLED'] = '1'
+    ENV['CGO_LDFLAGS'] = "-L#{prefix}/lib"
+    ENV['CGO_CFLAGS'] = "-I#{prefix}/include"
+    ENV['KS_API_URL'] = "https://develop---keystone-server-esk4nrfqlq-oa.a.run.app"
 
-    system 'go build -ldflags "-X github.com/wearedevx/keystone/cli/pkg/client.ApiURL=http://localhost:9000" -o ks'
-    bin.install "ks" => "ks"
+    system 'ls', "#{prefix}/include"
+
+    Dir.chdir 'cli' do
+      system Formula['go'].bin + 'go', 'build', '-ldflags' , "-X github.com/wearedevx/keystone/cli/pkg/client.ApiURL=#{ENV['KS_API_URL']}", '-o', 'ks'
+    end
+
+    bin.install "cli/ks" => "ks"
   end
 end
+
 
